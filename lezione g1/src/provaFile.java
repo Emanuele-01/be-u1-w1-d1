a145a517e2d31ac51a9a7d8ea615d337a051c73b
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-05-02 14:30:46 +0200

    inizializzazione repo

---
+++ lezione g1/src/provaFile.java
@@ -0,0 +1,14 @@
+
+public class provaFile {
+	
+	static void main(String[] args){
+		System.out.println("ciao");
+		
+		String x = "ciao";
+		
+		byte a = 1, b = 2, c = 3; 
+		
+		System.out.println(a + b + c);
+	}
+
+}
